7405503e9dea434edbe6824be3ac20cb26ebaabe
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-10-29 10:19:51 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/** Import librairy generare */
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//import ReactGA from 'react-ga';
+//import Cookies from 'universal-cookie';
+
+/** Include css */
 import './index.css';
-import App from './App';
+import './App.css';
+/** End */
+
+
+
+import RouterUser from "./router/RouterUser";
+
+/** Import redux */
+import { Provider } from "react-redux";
+import store from "./redux/store";
+/** End */
 import reportWebVitals from './reportWebVitals';
 
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <BrowserRouter>
+            <Route component={RouterUser} />
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('global-investissement')
 );
 
 // If you want to start measuring performance in your app, pass a function
